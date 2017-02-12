6e881f83defcf82a942ad9fdc91b5b6d882c5cb8
Ahmet Celikbas <celikbas.ahmet@gmail.com>
2017-02-12 19:24:47 +0100

    Change default tabs names to project tabs name
    home to maps
    about to share
    contact to shares list

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-list',
+  templateUrl: 'list.html'
 })
-export class HomePage {
+export class ListPage {
 
   constructor(public navCtrl: NavController) {
 
