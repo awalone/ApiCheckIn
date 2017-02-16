5057fb37059734339d891666cd51f2378530f51c
Ahmet Celikbas <celikbas.ahmet@gmail.com>
2017-02-16 13:58:47 +0100

    Change tab order to show shares lists first

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -9,9 +9,9 @@ import { ListPage } from '../list/list';
 export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  tab1Root: any = MapPage;
-  tab2Root: any = ListPage;
-
+  
+  tab1Root: any = ListPage;
+  tab2Root: any = MapPage;
   constructor() {
 
   }
